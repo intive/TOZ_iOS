9b24ded4247ab9b253c14195a994d55b77f77330
Gorczyk89 <gorczyk123@gmail.com>
2017-03-07 15:54:13 +0100

    Add TabBar and GalleryViewController, HelpViewController and NewsViewController.

---
+++ TOZ_iOS/GalleryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GalleryViewController.swift
+//  TOZ_iOS
+//
+//  Copyright © 2017 intive. All rights reserved.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ TOZ_iOS/NewsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewsViewController.swift
+//  TOZ_iOS
+//
+//  Copyright © 2017 intive. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
