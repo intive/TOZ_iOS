77272c9b8e8685eb5af280887063aaf1ee5e366b
RKB <rkb@Roberts-MacBook-Pro.local>
2017-03-08 17:47:24 +0100

    new datasource

---
+++ TOZ_iOS/News.swift
@@ -0,0 +1,20 @@
+//
+//  News.swift
+//  TOZ_iOS
+//
+//  Copyright © 2017 intive. All rights reserved.
+//
+
+import UIKit
+
+
+struct News {
+    
+    //Na ekranie urządzenia GOŚCIA/WOLONTARIUSZA każdy wyświetlany komunikat posiada: tytuł[wymagane], datę publikacji[wymagane], skróconą treść komunikatu[wymagane] oraz zdjęcie[niewymagane].
+    
+    var title:String
+    var dataPublish:Date
+    var messageShort:String
+    var picture:UIImage?
+    
+}
--- TOZ_iOS/NewsViewController.swift
+++ TOZ_iOS/NewsViewController.swift
@@ -8,10 +8,51 @@
 import UIKit
 
 class NewsViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableViewNews: UITableView!
+    var news:[News] =  [News(title: "Tytul", dataPublish: Date(), messageShort: "Message", picture: nil)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //tableViewNews.register(NewsTableViewCell.self, forCellReuseIdentifier: "news_cell")
+        tableViewNews.dataSource = self
+        tableViewNews.delegate = self
+        
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        tableViewNews.reloadData()
+        
+    }
+    
 }
 
+
+
+extension NewsViewController: UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return news.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell:NewsTableViewCell = tableView.dequeueReusableCell(withIdentifier: "news_cell", for: indexPath) as! NewsTableViewCell
+        
+        //cell configuration
+        cell.title.text = news[indexPath.row].title
+        cell.messageShort.text = news[indexPath.row].messageShort
+        cell.dataPublish.text = String(describing: news[indexPath.row].dataPublish)
+        return cell
+        
+    }
+    
+}
+
+extension NewsViewController: UITableViewDelegate {
+    
+}
