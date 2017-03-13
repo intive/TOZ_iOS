1747b5e8f41fd8b5b584ac227ab6434f855a6ee7
Piotr Tobolski <piotr.tobolski@intive.com>
2017-03-13 20:16:34 +0100

    Replace xcodebuild with xctool

--- travis/script.sh
+++ travis/script.sh
@@ -5,10 +5,8 @@ PLATFORM="platform=iOS Simulator,OS=10.2,name=iPhone 7"
 WORKSPACE="TOZ_iOS.xcworkspace"
 SCHEME="TOZ_iOS"
 
-xcodebuild \
+xctool \
     -workspace "$WORKSPACE" \
     -scheme "$SCHEME" \
 	-destination "$PLATFORM" \
-    -hideShellScriptEnvironment \
-	test \
-	| xcpretty
+	test
