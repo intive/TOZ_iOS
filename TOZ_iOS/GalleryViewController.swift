d74985fa97d64080bd4567f39e457d09306911e7
ubiquity7 <patronage@Trackmania.local>
2017-03-15 14:14:45 +0100

    Added photo download for photo caching and basic scheme for caching

--- TOZ_iOS/GalleryViewController.swift
+++ TOZ_iOS/GalleryViewController.swift
@@ -11,9 +11,22 @@ import Foundation
 class GalleryViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     let urlStr = "https://static.pexels.com/photos/347721/pexels-photo-347721.jpeg"
+    var url: NSURL! //
     //var url:URL! //
+    /*
+    var placeholderImageView: UIImageView!
+    var img:UIImage!
+    var refreshCtrl: UIRefreshControl!
+    var tableData:[AnyObject]!
+    var task: URLSessionDownloadTask!
+    var session: URLSession!
+    var cache:NSCache<AnyObject, AnyObject>!
+    */
     override func viewDidLoad() {
         super.viewDidLoad()
+        url = NSURL(string:urlStr)
+        let data = NSData(contentsOf: url as URL)
+        self.imageView.image = UIImage(data: data as Data!)
     }
 
 }
