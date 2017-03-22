28f4c85fd39712aa1de0b65d72da51f4e1692aa7
RKB <rkb@Roberts-MacBook-Pro.local>
2017-03-22 17:38:17 +0100

    resize if no picture

--- TOZ_iOS/NewsTableViewCell.swift
+++ TOZ_iOS/NewsTableViewCell.swift
@@ -27,6 +27,13 @@ class NewsTableViewCell: UITableViewCell {
         pictureInView.frame = CGRect(x: 0, y: 0, width: photoView.frame.width, height: photoView.frame.height)
         photoView.addSubview(pictureInView)
         photoView.backgroundColor = Color.Cell.Background.primary
+    
+        if news.picture == nil {
+            let lackOfPictuteConstraint:NSLayoutConstraint = NSLayoutConstraint(item: contentTextView, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.leadingMargin, multiplier: 1, constant: 10)
+            self.addConstraint(lackOfPictuteConstraint)
+        }
+        
     }
+    
     
 }
