45829ce89b286b24f7e77bfc68f2456599569ac1
RKB <rkb@Roberts-MacBook-Pro.local>
2017-03-23 16:20:32 +0100

    misspelled the name of class CalendarViewcontroller

--- TOZ_iOS/CalendarViewController.swift
+++ TOZ_iOS/CalendarViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CalemdarViewController: UIViewController {
+class CalendarViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
