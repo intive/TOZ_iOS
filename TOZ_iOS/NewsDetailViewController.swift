e26436c90216093898272cc31e614b3049ed4830
Gorczyk89 <gorczyk123@gmail.com>
2017-04-08 17:30:30 +0200

    Add clean NewsDetailViewController

---
+++ TOZ_iOS/NewsDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewsDetailViewController.swift
+//  TOZ_iOS
+//
+//  Copyright © 2017 intive. All rights reserved.
+//
+
+import UIKit
+
+class NewsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
