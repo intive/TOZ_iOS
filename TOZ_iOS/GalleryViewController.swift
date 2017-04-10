9c2dc79adf62f9b42dc83978c63d40bc141d4fc0
Marcin Kobiela <kobsonauta@89-64-146-83.dynamic.chello.pl>
2017-04-10 17:52:08 +0200

    Added constraints to HelpFinanceViewController

--- TOZ_iOS/GalleryViewController.swift
+++ TOZ_iOS/GalleryViewController.swift
@@ -23,7 +23,7 @@ class GalleryViewController: UIViewController, UITableViewDataSource, UITableVie
         let secondAnimal = GalleryEntity(name: "Asti", age: "5 lat", description: "Dog ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus.", image: UIImage(named: "dog2"))
         let thirdAnimal = GalleryEntity(name: "Lola", age: "4 lata", description: "Dog ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies.", image: UIImage(named: "dog3"))
         animalsArray = [firstAnimal, secondAnimal, thirdAnimal]
-        temporaryName.text = "TOZ Szczecin"
+//        temporaryName.text = "TOZ Szczecin"
     }
     // UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
