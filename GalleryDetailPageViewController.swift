fb66b11ed6c706698e460ac9cbf0db4af8ded4cf
Filip Zieliński <filipzielinski@filips-mbp.home>
2017-04-10 16:41:50 +0200

    Add pageviewcontroller to gallery detail view

--- GalleryDetailPageViewController.swift
+++ GalleryDetailPageViewController.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 
-class GalleryDetailPageViewController: UIPageViewController {
+class GalleryDetailPageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+
+    var pages = [UIViewController]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.delegate = self
+        self.dataSource = self
+        
+        let page1: UIViewController! = storyboard?.instantiateViewController(withIdentifier: "page1")
+        let page2: UIViewController! = storyboard?.instantiateViewController(withIdentifier: "page2")
+        
+        pages.append(page1)
+        pages.append(page2)
+        
+        setViewControllers([page1], direction: UIPageViewControllerNavigationDirection.forward, animated: false, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,7 +30,32 @@ class GalleryDetailPageViewController: UIPageViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let currentIndex = pages.index(of: viewController)!
+        let previousIndex = abs((currentIndex - 1) % pages.count)
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let currentIndex = pages.index(of: viewController)!
+        let nextIndex = abs((currentIndex + 1) % pages.count)
+        return pages[nextIndex]
+    }
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return pages.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+    
     /*
+     
+     public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?
+     
+     @available(iOS 5.0, *)
+     public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
