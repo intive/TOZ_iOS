f8356a320656c8d6453c8b36072b03a8cbd9de58
Filip Zieliński <filipzielinski@filips-mbp.home>
2017-04-11 20:37:39 +0200

    Add missing segue for profile photo

--- TOZ_iOS/GalleryDetailViewController.swift
+++ TOZ_iOS/GalleryDetailViewController.swift
@@ -17,6 +17,7 @@ class GalleryDetailViewController: UIViewController {
     @IBOutlet weak var animalDescription: UITextView!
 
     var selectedCell: String?
+    var photoURL: URL?
     var animalOperation: AnimalOperation?
 
     func makeAnimalOperation() {
@@ -43,11 +44,19 @@ class GalleryDetailViewController: UIViewController {
                     self.animalSex.text = localAnimal.sex
                     self.animalCreationDate.text = localAnimal.created?.dateToFormattedString()
                     self.animalDescription.text = localAnimal.description
+                    self.photoURL = localAnimal.imageUrl
                 }
             case .failure(let error):
                 print ("\(error)")
             }
         }
         animalOperation?.start()
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "profilePhotoSegue" {
+            let GalleryDetailPhotoViewController = segue.destination as? GalleryDetailPhotoViewController
+            GalleryDetailPhotoViewController?.galleryDetailPhotoURL = photoURL
+        }
     }
 }
