2251b62d43ea5c0931517e870160e4463d66b4d7
ubiquity7 <patronage@Trackmania.local>
2017-04-13 12:14:50 +0200

    Deleted unnecessary block of code, added url.path comparison

--- TOZ_iOS/NetworkServiceMock.swift
+++ TOZ_iOS/NetworkServiceMock.swift
@@ -14,12 +14,11 @@ class NetworkServiceMock: NetworkService {
                               params: [String : Any]?,
                               headers: [String : String]?,
                               success: ((Data?) -> Void)?,
-                              failure: ((Data?, RequestError, Int) -> Void)?) {
+                              failure: ((_ data: Data?, _ error: RequestError, _ responseCode: Int) -> Void)? = nil) {
         var jsonObj: NSData?
         var path: String?
         // News request
-        if url.absoluteString == "http://dev.patronage2017.intive-projects.com/news?shortened=false" ||
-            url.absoluteString == "http://dev.patronage2017.intive-projects.com/news?shortened=true" {
+        if url.absoluteString == url.path {
             switch method {
             case .GET:
                 path = Bundle.main.path(forResource: "GetNews", ofType: "json")
@@ -27,7 +26,7 @@ class NetworkServiceMock: NetworkService {
             default:
                 failure?(nil, RequestError.InvalidRequest, 0)
             } // organization info request
-        } else if url.absoluteString == "http://dev.patronage2017.intive-projects.com/organization/info" {
+        } else if url.absoluteString == url.path {
             switch method {
             case .GET:
                 path = Bundle.main.path(forResource: "GetOrganizationInfo", ofType: "json")
@@ -35,7 +34,7 @@ class NetworkServiceMock: NetworkService {
             default:
                 failure?(nil, RequestError.InvalidRequest, 0)
             } // animal list request
-        } else if url.absoluteString == "http://dev.patronage2017.intive-projects.com/pets" {
+        } else if url.absoluteString == url.path {
             switch method {
             case .GET:
                 path = Bundle.main.path(forResource: "GetAnimals", ofType: "json")
@@ -47,46 +46,5 @@ class NetworkServiceMock: NetworkService {
         if let path = path {
             jsonObj = try? NSData(contentsOfFile: path, options: NSData.ReadingOptions.mappedIfSafe)
         }
-        var mutableRequest = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 10.0)
-        mutableRequest.allHTTPHeaderFields = headers
-        mutableRequest.httpMethod = method.rawValue
-        if let jsonObj = jsonObj {
-            do {
-                mutableRequest.httpBody = try JSONSerialization.data(withJSONObject: jsonObj, options: [])
-            } catch {
-                print("Serialization parameters to JSON failed")
-                failure?(nil, RequestError.FailedToSerializeJSON, 0)
-                return
-            }
-        }
-        /// Creates a session with default configuration
-        let session = URLSession.shared
-        /// Creates task object within the session. Returns data as an Data object
-        task = session.dataTask(with: mutableRequest as URLRequest, completionHandler: { (data, response, error) in
-            guard let httpResponse = response as? HTTPURLResponse else {
-                failure?(data, RequestError.InvalidResponse, 0)
-                return
-            }
-            if let error = error {
-                // Request failed, might be internet connection issue
-                failure?(data, RequestError.ConnectionError(error), httpResponse.statusCode)
-                return
-            }
-            if self.successCodes.contains(httpResponse.statusCode) {
-                print("Request finished with success.")
-                success?(data)
-            } else if self.failureCodes.contains(httpResponse.statusCode) {
-                print("Request finished with failure.")
-                failure?(data, RequestError.InvalidRequest, httpResponse.statusCode)
-            } else {
-                // Server returned response with status code different than
-                // expected `successCodes`
-                print("Request finished with serious failure.")
-                print("Request failed with code \(httpResponse.statusCode)")
-                print("Wrong handling logic, wrong endpoint mapping or backend bug.")
-                failure?(data, RequestError.UnexpectedNetworkError, httpResponse.statusCode)
-            }
-        })
-        task?.resume()
     }
 }
