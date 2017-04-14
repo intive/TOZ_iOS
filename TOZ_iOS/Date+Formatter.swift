bee68e772c6b794f83ca23d19ef816c3a6967131
Gorczyk89 <gorczyk123@gmail.com>
2017-04-14 23:24:17 +0200

    Function to show how much time passed since news publish date

--- TOZ_iOS/Date+Formatter.swift
+++ TOZ_iOS/Date+Formatter.swift
@@ -12,6 +12,21 @@ extension Date {
     func dateToFormattedString() -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
-        return formatter.string(from: self)
+        let calendar = Calendar.current
+        let now = Date()
+        let components = calendar.dateComponents([.day, .hour, .minute], from: self, to: now as Date)
+        var result: String = ""
+
+        if components.day! <= 1 {
+            result = "\(components.hour!) godzin temu"
+        } else if components.hour! <= 1 {
+            result = "\(components.minute!)) minut temu"
+        } else if components.minute! <= 1 {
+            result = "przed chwilą"
+        } else {
+            result = formatter.string(from: self)
+        }
+
+        return result
     }
 }
