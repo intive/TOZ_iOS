8eaf9e179f60402abae19e6e07745b90520e0107
Gorczyk89 <gorczyk123@gmail.com>
2017-04-15 19:59:41 +0200

    Unwrapping date components fixed

--- TOZ_iOS/Date+Formatter.swift
+++ TOZ_iOS/Date+Formatter.swift
@@ -14,14 +14,17 @@ extension Date {
         formatter.dateFormat = "yyyy-MM-dd"
         let calendar = Calendar.current
         let now = Date()
-        let components = calendar.dateComponents([.day, .hour, .minute], from: self, to: now as Date)
+        let components = calendar.dateComponents([.day, .hour, .minute], from: self, to: now)
         var result: String = ""
+        let day = components.day ?? 0
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
 
-        if components.day! <= 1 {
-            result = "\(components.hour!) godzin temu"
-        } else if components.hour! <= 1 {
-            result = "\(components.minute!)) minut temu"
-        } else if components.minute! <= 1 {
+        if day <= 1 {
+            result = "\(components.hour) godzin temu"
+        } else if hour <= 1 {
+            result = "\(components.minute)) minut temu"
+        } else if minute <= 1 {
             result = "przed chwilą"
         } else {
             result = formatter.string(from: self)
