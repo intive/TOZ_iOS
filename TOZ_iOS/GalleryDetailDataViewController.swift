52a2a12f657634136c21816c9970f4ba81a05116
Filip Zieliński <fzielinski@gmail.com>
2017-04-18 21:08:17 +0200

    Remove strong reference to self in closure expression Change dataObject type to URL

--- TOZ_iOS/GalleryDetailDataViewController.swift
+++ TOZ_iOS/GalleryDetailDataViewController.swift
@@ -12,13 +12,25 @@ class GalleryDetailDataViewController: UIViewController {
     @IBOutlet weak var dataImage: UIImageView!
     @IBOutlet weak var photoCaption: UILabel!
 
-    var dataObject: UIImage = #imageLiteral(resourceName: "placeholder")
+    var dataObject: URL?
     var photoIndex: Int = 0
     var photosCount: Int = 0
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.dataImage!.image = dataObject
+        getAnimalPhoto()
         self.photoCaption.text = "Zdjęcie \(photoIndex) / \(photosCount)"
+    }
+
+    func getAnimalPhoto() {
+            if let dataObject = self.dataObject {
+                DispatchQueue.main.async {
+                    PhotoManager.shared.getPhoto(from: dataObject, completion: { [weak self] (image) -> (Void) in
+                    self?.dataImage.image = image
+                })
+                }
+            } else {
+        self.dataImage.image = #imageLiteral(resourceName: "placeholder")
+        }
     }
 }
