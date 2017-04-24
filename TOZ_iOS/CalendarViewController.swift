57d7e87822268cb37f55888b6d5b684d55d197d8
Robert Karbowiak <rkkcorp@poczta.onet.pl>
2017-04-24 16:01:50 +0200

    fixes in line wth the reviews

--- TOZ_iOS/CalendarViewController.swift
+++ TOZ_iOS/CalendarViewController.swift
@@ -9,22 +9,22 @@ import Foundation
 
 class CalendarViewController: UIViewController {
 
-    @IBOutlet weak var prevButtonVar: UIButton!
+    @IBOutlet weak var prevButton: UIButton!
 
-    @IBOutlet weak var nextButtonVar: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
 
-    @IBOutlet weak var currDateLabel: UILabel!
+    @IBOutlet weak var currentDateLabel: UILabel!
 
     var pageController: UIPageViewController!
     var weekPages = [WeekViewController]()
     private var indexPage = 0
 
     @IBAction func nextWeek(_ sender: Any) {
-       pageController.setViewControllers([nextWeekController()], direction: .forward, animated: true, completion: nil)
+       pageController.setViewControllers([nextWeekController()], direction: .forward, animated: true)
     }
 
     @IBAction func previousWeek(_ sender: Any) {
-        pageController.setViewControllers([nextWeekController()], direction: .reverse, animated: true, completion: nil)
+        pageController.setViewControllers([nextWeekController()], direction: .reverse, animated: true)
     }
 
     func nextWeekController() -> WeekViewController {
@@ -37,7 +37,7 @@ class CalendarViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // swiftlint:disable:next force_cast
-        pageController = self.childViewControllers.first as! UIPageViewController
+        //pageController = self.childViewControllers.first as! UIPageViewController
         pageController.dataSource = self
         // swiftlint:disable:next force_cast
         let weekAfter: WeekViewController = self.storyboard?.instantiateViewController(withIdentifier: "WeekViewController") as! WeekViewController
@@ -47,24 +47,22 @@ class CalendarViewController: UIViewController {
         weekPages.append(weekBefore)
         weekPages.append(weekAfter)
         //set initial view
-        pageController.setViewControllers([weekBefore], direction: .forward, animated: true, completion: nil)
+        pageController.setViewControllers([weekBefore], direction: .forward, animated: true)
     }
 
     override func viewDidLayoutSubviews() {
-        configureLayout()
+        prevButton.layer.cornerRadius = prevButton.bounds.height * 0.5
+        prevButton.setTitleColor(UIColor.darkGray, for: .normal)
+        prevButton.backgroundColor = UIColor.lightGray
+
+        nextButton.layer.cornerRadius = nextButton.bounds.height * 0.5
+        nextButton.setTitleColor(UIColor.white, for: .normal)
+        nextButton.backgroundColor = UIColor.darkText
     }
 
-    func configureLayout() {
-        prevButtonVar.layoutIfNeeded()
-        prevButtonVar.layer.cornerRadius = prevButtonVar.bounds.height * 0.5
-        prevButtonVar.setTitleColor(UIColor.darkGray, for: .normal)
-        prevButtonVar.backgroundColor = UIColor.lightGray
-
-        nextButtonVar.layoutIfNeeded()
-        nextButtonVar.layer.cornerRadius = nextButtonVar.bounds.height * 0.5
-        nextButtonVar.setTitleColor(UIColor.white, for: .normal)
-        nextButtonVar.backgroundColor = UIColor.darkText
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // swiftlint:disable:next force_cast
+        pageController = segue.destination as! UIPageViewController
     }
 
 }
---
+++ TOZ_iOS/ScheduleControl.swift
@@ -0,0 +1,45 @@
+//
+//  ScheduleControl.swift
+//  TOZ_iOS
+//
+//  Copyright © 2017 intive. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleControl: UIControl {
+
+    @IBOutlet var view: UIView!
+    @IBOutlet weak var scheduleLabel: UILabel!
+
+    var scheduleSelected: Bool = false {
+        didSet {
+            if scheduleSelected {
+                scheduleLabel.layer.backgroundColor = UIColor.lightGray.cgColor
+                scheduleLabel.textColor = UIColor.darkText
+            } else {
+                scheduleLabel.layer.backgroundColor = UIColor.white.cgColor
+                scheduleLabel.textColor = UIColor.white
+            }
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        Bundle.main.loadNibNamed("ScheduleControl", owner: self, options: nil)
+        addSubview(view)
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        view.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        view.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        view.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+    }
+
+    override func layoutSubviews() {
+        scheduleLabel.layer.cornerRadius = scheduleLabel.bounds.height * 0.5
+        scheduleLabel.layer.borderWidth = 1
+        scheduleLabel.layer.borderColor = UIColor.gray.cgColor
+    }
+
+}
