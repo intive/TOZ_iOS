8c9cda6f43237c6c70135c78a60d67cc1e84b0b9
Filip Zieliński <fzielinski@gmail.com>
2017-04-25 21:35:38 +0200

    Add User class for storing user’s informations

--- TOZ_iOS/User.swift
+++ TOZ_iOS/User.swift
@@ -2,8 +2,21 @@
 //  User.swift
 //  TOZ_iOS
 //
-//  Created by Filip Zieliński on 25/04/2017.
 //  Copyright © 2017 intive. All rights reserved.
 //
 
 import Foundation
+
+final class User {
+    private(set) var isSignedIn: Bool
+
+    static var shared: User = User(token: BackendAuth.shared.token)
+
+    init(token: String?) {
+        if token != nil {
+            self.isSignedIn = true
+        } else {
+            self.isSignedIn = false
+        }
+    }
+}
