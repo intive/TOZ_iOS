ed196485a01e46fb98e4b34a33d2b3783119107b
Robert Karbowiak <rkkcorp@poczta.onet.pl>
2017-05-09 10:08:15 +0200

    add WeekdayItem struct

---
+++ TOZ_iOS/WeekdayItem.swift
@@ -0,0 +1,15 @@
+//
+//  WeekdayItem.swift
+//  TOZ_iOS
+//
+//  Copyright © 2017 intive. All rights reserved.
+//
+
+import Foundation
+
+struct WeekdayItem {
+    var day: String
+    var month: String
+    var year: String
+    var dataLabel: String
+}
