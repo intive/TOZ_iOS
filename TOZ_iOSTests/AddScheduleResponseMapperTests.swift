fa5bf0f7f2a85311a8b99c07ea717e8d6dd509f7
Robert Karbowiak <rkkcorp@poczta.onet.pl>
2017-05-09 22:39:56 +0200

    remain check response mapper remove other tests

--- TOZ_iOSTests/AddScheduleResponseMapperTests.swift
+++ TOZ_iOSTests/AddScheduleResponseMapperTests.swift
@@ -11,25 +11,41 @@ import XCTest
 class AddScheduleResponseMapperTests: XCTestCase {
 
     var reservationItem: ReservationItem?
-    var addScheduleOperation: AddScheduleOperation?
 
     override func setUp() {
+        var responseData: NSData?
+        var json: Any?
 
-        let newItem = ReservationItem.init(idObject: "", date: Date(), timeOfDay: .morning, ownerSurname: "Doe", ownerForename: "John")
-        addScheduleOperation = AddScheduleOperation(dataObject: newItem, modificationMessage: "string")
-        addScheduleOperation?.service.service = NetworkServiceMock()
-        addScheduleOperation?.resultCompletion = { result in
-                switch result {
-                case .success(let successResponse):
-                    self.reservationItem = successResponse
-                case .failure(let error):
-                    XCTFail("\(error)")
-                }
-            }
+        let path = Bundle.main.path(forResource: "AddSchedule", ofType: "json")
+        if let path = path {
+            responseData = try? NSData(contentsOfFile: path, options: [])
+        }
+        if let responseData = responseData {
+            json = try? JSONSerialization.jsonObject(with: responseData as Data, options: [])
+        }
+        reservationItem = try? GetScheduleResponseMapper.process(json as AnyObject)
     }
 
-    func testAddScheduleOperation() {
-        addScheduleOperation?.start()
+    func testReservationItemID() {
+        XCTAssertEqual(self.reservationItem?.idObject, "111111-347f-4b2e-b1c6-6faff971f767")
+    }
+
+    func testReservationItemTimeOfDay() {        XCTAssertEqual(self.reservationItem?.timeOfDay, TimeOfDay.morning)
+    }
+
+    func testReservationItemDate() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        formatter.timeZone = TimeZone(abbreviation: "UTC")
+        XCTAssertEqual(formatter.string(from: (self.reservationItem!.date)), "2017-01-21")
+    }
+
+    func testReservationItemOwnerForename() {
+        XCTAssertEqual(self.reservationItem?.ownerForename, "John")
+    }
+
+    func testReservationItemOwnerSurname() {
+        XCTAssertEqual(self.reservationItem?.ownerSurname, "Doe")
     }
 
 }
