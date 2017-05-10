6c191aed9de0b4296d30701c3f5882d5c8e81c67
Robert Karbowiak <rkkcorp@poczta.onet.pl>
2017-05-10 12:30:43 +0200

    Some fixes due to review add conformance to Equatable move date conversion to struct extension

--- TOZ_iOS/CalendarHelper.swift
+++ TOZ_iOS/CalendarHelper.swift
@@ -9,29 +9,21 @@ import Foundation
 
 class CalendarHelper {
 
-    var currentCalendar: Calendar!
+    //var currentCalendar: Calendar!
+    let calendar = Calendar.current
     var currentMonday: Date!
 
-    init() {
-        currentCalendar = Calendar.current
-        let currentWeek = currentCalendar.component(.weekday, from: Date())
-        let distanceFromMonday = (currentWeek != 1 ? currentWeek - 2 : 7)
-        currentMonday = currentCalendar.date(byAdding: .day, value: -distanceFromMonday, to: Date())!
-    }
-
-    func isDatesEqual(fistDate: WeekdayItem, with secondDate: WeekdayItem) -> Bool {
-        if fistDate.day == secondDate.day && fistDate.month == secondDate.month && fistDate.year == secondDate.year {
-            return true
-        } else {
-            return false
-        }
+    init(date: Date = Date()) {
+        let currentWeekday = calendar.component(.weekday, from: date)
+        let distanceFromMonday = (currentWeekday != 1 ? currentWeekday - 2 : 6)
+        currentMonday = calendar.date(byAdding: .day, value: -distanceFromMonday, to: date)!
     }
 
     func weekdayItemArray() -> [WeekdayItem] {
 
         var daysInWeek: [WeekdayItem] = []
         for i in 0...6 {
-            let addNewItem = weekdayItem(from: currentCalendar.date(byAdding: .day, value: i, to: currentMonday)!)
+            let addNewItem = weekdayItem(from: calendar.date(byAdding: .day, value: i, to: currentMonday)!)
             daysInWeek.append(addNewItem)
         }
 
@@ -39,54 +31,32 @@ class CalendarHelper {
     }
 
     func weekdayItem(from date: Date) -> WeekdayItem {
-        var components = currentCalendar.dateComponents([.year, .month, .day], from: date)
-        var dayComponent: Int = 0
-        var monthComponent: Int = 0
-        var yearComponent: Int = 0
-        var dataLabel: String
-        var day, month, year: String
-        if let check = components.day {
-            dayComponent = check
-        }
-        if let check = components.month {
-            monthComponent = check
-        }
-        if let check = components.year {
-            yearComponent = check
-        }
-        if dayComponent < 10 {
-            day = "0" + String(dayComponent)
-        } else {
-            day = "\(dayComponent)"
-        }
-        if monthComponent < 10 {
-            month = "0\(monthComponent)"
-        } else {
-            month = "\(monthComponent)"
-        }
-        year = "\(yearComponent)"
-        dataLabel = "\(year)-\(month)-\(day)"
+        var components = calendar.dateComponents([.year, .month, .day], from: date)
 
-        let dateWeekdayItem = WeekdayItem(day: day, month: month, year: year, dataLabel: dataLabel)
+        let day = String(format: "%02d", components.day!)
+        let month = String(format: "%02d", components.month!)
+        let year = String(format: "%d", components.year!)
+
+        let dateWeekdayItem = WeekdayItem(day: day, month: month, year: year)
 
         return dateWeekdayItem
     }
 
     func date(from weekdayItem: WeekdayItem) -> Date {
-        var components = currentCalendar.dateComponents([.year, .month, .day], from: currentMonday)
+        var components = calendar.dateComponents([.year, .month, .day], from: currentMonday)
         components.day = Int(weekdayItem.day)
         components.month = Int(weekdayItem.month)
         components.year = Int(weekdayItem.year)
 
-        return currentCalendar.date(from: components)!
+        return calendar.date(from: components)!
     }
 
     func nextWeek() {
-        currentMonday = currentCalendar.date(byAdding: .weekOfYear, value: 1, to: currentMonday)!
+        currentMonday = calendar.date(byAdding: .weekOfYear, value: 1, to: currentMonday)!
     }
 
     func previousWeek() {
-        currentMonday = currentCalendar.date(byAdding: .weekOfYear, value: -1, to: currentMonday)!
+        currentMonday = calendar.date(byAdding: .weekOfYear, value: -1, to: currentMonday)!
     }
 
 }
