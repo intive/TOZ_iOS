d37047c51d7feb57725bd57ace9e473261cf9bf1
Filip Zieliński <fzielinski@gmail.com>
2017-05-11 20:05:43 +0200

    Add TabBarDelegate protocol

---
+++ TOZ_iOS/TabBarDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  TabBarDelegate.swift
+//  TOZ_iOS
+//
+//  Copyright © 2017 intive. All rights reserved.
+//
+
+import Foundation
+
+protocol TabBarDelegate: class {
+    func switchAccountTab()
+}
