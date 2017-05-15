ab5fecb7c51b42410186b9eaafc69881894cdd75
KTylke <k.tylke7@gmail.com>
2017-05-15 18:25:18 +0200

    Add SignUpViewController file

--- TOZ_iOS/SignUpViewController.swift
+++ TOZ_iOS/SignUpViewController.swift
@@ -10,26 +10,54 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    let options = ["Chcę zostać wolontariuszem", "Chcę zostać domem tymczasowym"]
+
+    @IBOutlet weak var firstNameInput: TextInputView!
+    @IBOutlet weak var surnameInput: TextInputView!
+    @IBOutlet weak var emailInput: TextInputView!
+    @IBOutlet weak var phoneNumberInput: TextInputView!
+
+    @IBOutlet weak var goalSegmentedControl: UISegmentedControl!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelButtonTapped(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "unwindToLoginView", sender: self)
     }
-    */
 
+    func configureView() {
+        self.view.backgroundColor = Color.SignUpViewController.background
+        firstNameInput.textChecker = NameChecker()
+        surnameInput.textChecker = NameChecker()
+        emailInput.textChecker = EmailChecker()
+        phoneNumberInput.textChecker = PhoneNumberChecker()
+
+        goalSegmentedControl.backgroundColor = Color.SignUpViewController.SegmentedControl.background
+        goalSegmentedControl.tintColor = Color.SignUpViewController.SegmentedControl.tint
+
+        goalSegmentedControl.layer.cornerRadius = 5
+
+        firstNameInput.placeholder = " Imię..."
+        surnameInput.placeholder = " Nazwisko..."
+        emailInput.placeholder = " E-mail..."
+        phoneNumberInput.placeholder = " Numer telefonu..."
+    }
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
