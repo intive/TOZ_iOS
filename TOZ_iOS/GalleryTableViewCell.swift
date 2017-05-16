32fa80ac09cb51a780f255fa6cea7f9700cd316d
Rafal Gorczynski <rafalgorczynski@Rafals-MacBook-Pro.local>
2017-05-17 00:26:24 +0200

    Layout changes to GalleryTableViewCell

--- TOZ_iOS/GalleryTableViewCell.swift
+++ TOZ_iOS/GalleryTableViewCell.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class GalleryTableViewCell: UITableViewCell {
-    @IBOutlet weak var animalImage: UIImageView!
+    @IBOutlet weak var animalImage: ProfilePhotoView!
+    @IBOutlet weak var animalImageWidth: NSLayoutConstraint!
     @IBOutlet weak var animalName: UILabel!
     @IBOutlet weak var animalType: UILabel!
 
@@ -18,6 +19,8 @@ class GalleryTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         self.backgroundColor = Color.Cell.Background.secondary
+        animalImageWidth.constant = self.frame.width * 0.375
+        animalName.adjustsFontSizeToFitWidth = true
     }
 
     func configure(for animal: AnimalItem) {
@@ -25,11 +28,11 @@ class GalleryTableViewCell: UITableViewCell {
         self.animalName.text = animal.name
         self.animalType.text = animal.type
         let imageUrl: URL? = animal.imageUrl
-        animalImage.image = nil
+        animalImage.photo = nil
         if let imageUrl = imageUrl {
             PhotoManager.shared.getPhoto(from: imageUrl, completion: {(image) -> (Void) in
                 if self.animalID == animal.animalID {
-                    self.animalImage.image = image
+                    self.animalImage.photo = image
                 }
             })
         }
