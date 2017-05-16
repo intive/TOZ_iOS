5e7ca8099d0ae37971fda65ccc214f2502a94e5e
Filip Zieliński <fzielinski@gmail.com>
2017-05-16 21:43:56 +0200

    Configure UIPageViewController when array of photos is set

--- TOZ_iOS/GalleryDetailPhotoViewController.swift
+++ TOZ_iOS/GalleryDetailPhotoViewController.swift
@@ -9,33 +9,27 @@ import UIKit
 
 class GalleryDetailPhotoViewController: UIViewController, UIPageViewControllerDelegate {
 
-    var galleryDetailPhotoURL: URL?
-    var photos: [URL]?
     var pageViewController: UIPageViewController?
-
     lazy var modelController = GalleryDetailModelController()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Configure the page view controller and add it as a child view controller.
-        self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
-        self.pageViewController!.delegate = self
+    var photos: [URL] = [] {
+        didSet {
+            // Configure the page view controller and add it as a child view controller.
+            self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+            self.pageViewController!.delegate = self
 
-        if let photos = photos {
             self.modelController.pageData = photos
-        } else {
-            self.modelController.pageData = []
+
+            let startingViewController: GalleryDetailDataViewController = self.modelController.viewController(at: 0, storyboard: self.storyboard!)!
+            let viewControllers = [startingViewController]
+            self.pageViewController!.setViewControllers(viewControllers, direction: .forward, animated: false)
+
+            self.pageViewController!.dataSource = self.modelController
+
+            self.addChildViewController(self.pageViewController!)
+            self.view.addSubview(self.pageViewController!.view)
+
+            self.pageViewController!.didMove(toParentViewController: self)
         }
-
-        let startingViewController: GalleryDetailDataViewController = self.modelController.viewController(at: 0, storyboard: self.storyboard!)!
-        let viewControllers = [startingViewController]
-        self.pageViewController!.setViewControllers(viewControllers, direction: .forward, animated: false)
-
-        self.pageViewController!.dataSource = self.modelController
-
-        self.addChildViewController(self.pageViewController!)
-        self.view.addSubview(self.pageViewController!.view)
-
-        self.pageViewController!.didMove(toParentViewController: self)
     }
 }
