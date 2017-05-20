640ee028e0c3039a3bad1f8afbbdbb48773a52da
Rafal Gorczynski <rafalgorczynski@Rafals-MacBook-Pro.local>
2017-05-20 22:44:45 +0200

    Return information if request was sent succesfull

--- TOZ_iOS/ResetPasswordOperation.swift
+++ TOZ_iOS/ResetPasswordOperation.swift
@@ -10,20 +10,26 @@ import Foundation
 class ResetPasswordOperation: ServiceOperation {
 
     private let request: ResetPasswordRequest
+    var resultCompletion: ((_ success: Bool) -> ())?
+
 
     init(email: String) {
         request = ResetPasswordRequest(email: email)
         super.init()
     }
 
-    func start() {
+    func start(completion: ((_ success: Bool)->())?) {
+        self.resultCompletion = completion
         service.request(request, completion: handleResponse)
     }
 
     private func handleResponse(_ response: RequestResult<AnyObject>) {
         switch response {
-        case .success: return
-        case .failure: print ("error")
+        case .success:
+            resultCompletion?(true)
+            return
+        case .failure:
+            resultCompletion?(false)
         }
     }
 
