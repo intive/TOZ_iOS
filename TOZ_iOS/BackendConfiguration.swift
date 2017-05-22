debdbc16c09d0d6958b458d977158152c81304a4
Filip Zieliński <fzielinski@gmail.com>
2017-05-22 18:13:01 +0200

    Build URLs to images with correct path

--- TOZ_iOS/BackendConfiguration.swift
+++ TOZ_iOS/BackendConfiguration.swift
@@ -12,10 +12,12 @@ import Foundation
 final class BackendConfiguration {
 
     let baseURL: URL
+    let photosURL: URL
 
-    init(baseURL: URL) {
+    init(baseURL: URL, photosURL: URL) {
         self.baseURL = baseURL
+        self.photosURL = photosURL
     }
 
-    static var shared: BackendConfiguration = mockBackend ? BackendConfiguration(baseURL: URL(string: "https://i.imgur.com")!) : BackendConfiguration(baseURL: URL(string: "http://dev.patronage2017.intive-projects.com/api")!)
+    static var shared: BackendConfiguration = mockBackend ? BackendConfiguration(baseURL: URL(string: "https://i.imgur.com")!, photosURL: URL(string: "https://i.imgur.com")!) : BackendConfiguration(baseURL: URL(string: "http://dev.patronage2017.intive-projects.com/api")!, photosURL: URL(string: "http://dev.patronage2017.intive-projects.com/")!)
 }
