44e6f985c808069be52cb851e8ac746670a9a409
Filip Zieliński <filipzielinski@filips-mbp.home>
2017-06-17 15:25:09 +0200

    Change animal's type and sex to corresponding enum

--- TOZ_iOS/AnimalItem.swift
+++ TOZ_iOS/AnimalItem.swift
@@ -7,11 +7,35 @@
 
 import Foundation
 
+enum AnimalType {
+    case DOG, CAT
+    var localizedType: String {
+        switch self {
+        case .DOG:
+            return "Pies"
+        case .CAT:
+            return "Kot"
+        }
+    }
+}
+
+enum AnimalSex {
+    case MALE, FEMALE
+    var localizedSex: String {
+        switch self {
+        case .MALE:
+            return "On"
+        case .FEMALE:
+            return "Ona"
+        }
+    }
+}
+
 struct AnimalItem: ParsedItem {
     let animalID: String
     let name: String
-    let type: String
-    let sex: String
+    let type: AnimalType
+    let sex: AnimalSex?
     let description: String?
     let address: String?
     let created: Date?
